c2e8d5a41dba4481468b2c61d16288f3f7ff5beb
EmilianoEmanuelSosa <emilianoemanuel2850@gmail.com>
2023-10-18 13:01:24 -0300

    <MD> Files

--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -1,9 +1,6 @@
-return {
-	"wuelnerdotexe/vim-enfocado",
-	lazy=false,
-	priority=1000,
-	config=function()
-	-- load the colorscheme here
-	   vim.cmd([[colorscheme enfocado]])
-   end,
+return{
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
 }
