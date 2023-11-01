c82d71c546b8f2e3e6ea360ec05667175ee8db18
EmilianoEmanuelSosa <emilianoemanuel2850@gmail.com>
2023-11-01 14:12:36 -0300

    add  all

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 require("config")
+
 -- Cargar el complemento del esquema de colores Tokyo Night
 vim.cmd('colorscheme tokyonight-night')
 
+-- Establecer la opacidad de toda la ventana en Neovim
+vim.cmd('highlight Normal guibg=#00000070') -- Cambia el color de fondo para ajustar la opacidad al 50%
 
+
+vim.o.showtabline = 0
+vim.o.laststatus = 2
+vim.o.cmdheight = 1
